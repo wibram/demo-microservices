3ed9618443d50d5268b9d65c13b78bf06a6fe528
Ibrahim <Ibrahim@I10>
2021-09-30 17:31:37 +0200

    migration in api

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.API
 {
@@ -13,7 +11,19 @@ namespace Ordering.API
     {
         public static void Main( string[] args )
         {
-            CreateHostBuilder( args ).Build().Run();
+            //CreateHostBuilder( args ).Build().Run();
+
+            CreateHostBuilder( args )
+                .Build()
+                .MigrateDatabase<OrderContext>( (context, service) =>
+                {
+                    var logger = service.GetService<ILogger<OrderContextSeed>>();
+
+                    OrderContextSeed
+                        .SeedAsync( context, logger )
+                        .Wait();
+                })
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder( string[] args ) =>
@@ -21,6 +31,6 @@ namespace Ordering.API
                 .ConfigureWebHostDefaults( webBuilder =>
                  {
                      webBuilder.UseStartup<Startup>();
-                 } );
+                 });
     }
 }
