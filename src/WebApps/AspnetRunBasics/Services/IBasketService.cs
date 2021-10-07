dc95152e015bb5a6f78100dc5744b7395565d331
Ibrahim <Ibrahim@I10>
2021-10-07 11:36:29 +0200

    aspnetrunbasics web app

---
+++ src/WebApps/AspnetRunBasics/Services/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket( string userName );
+
+        Task<BasketModel> UpdateBasket( BasketModel model );
+
+        Task CheckoutBasket( BasketCheckoutModel model );
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService( HttpClient client )
+        {
+            _httpClient = client ?? throw new ArgumentNullException( nameof( client ) );
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUsername( string userName )
+        {
+            var response = await _httpClient.GetAsync( $"/Order/{userName}" );
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
